639d5960a87d471720e27f56964acad314758fa3
Peter Fortuin <peter.fortuin@gmail.com>
2020-10-30 20:41:54 +0100

    FlowSwitch should not collect values from inputFlow when nobody is collecting values from outputFlow

--- src/commonMain/kotlin/com/amuza/kotlin/flowvalve/FlowSwitch.kt
+++ src/commonMain/kotlin/com/amuza/kotlin/flowvalve/FlowSwitch.kt
@@ -9,23 +9,51 @@ class FlowSwitch<T, E : Enum<E>>(
     private val defaultValue: T,
     private val coroutineScope: CoroutineScope
 ) {
-
+    private var subscribersCount = 0
     private val _outFlow = MutableStateFlow(defaultValue)
-    val outFlow: StateFlow<T> = _outFlow
+    val outFlow: Flow<T> = _outFlow
+        .onStart {
+            subscribersCount++
+
+            if (subscribersCount == 1) {
+                startCollectingFromInputFlow()
+            }
+        }
+        .onCompletion {
+            subscribersCount--
+
+            if (subscribersCount == 0) {
+                cancelCollectionFromInputFlow()
+            }
+        }
 
     private val switchMap = HashMap<E, Flow<T>>()
     private var runningJob: Job? = null
+    private var selectedInput: E? = null
 
     fun selectInput(input: E) {
-        runningJob?.cancel()
+        selectedInput = input
+
+        cancelCollectionFromInputFlow()
 
         if (!switchMap.containsKey(input)) {
             _outFlow.value = defaultValue
+            return
         }
 
-        runningJob = switchMap[input]?.onEach {
+        if (subscribersCount > 0) {
+            startCollectingFromInputFlow()
+        }
+    }
+
+    private fun startCollectingFromInputFlow() {
+        runningJob = switchMap[selectedInput]?.onEach {
             _outFlow.value = it
         }?.launchIn(coroutineScope)
+    }
+
+    private fun cancelCollectionFromInputFlow() {
+        runningJob?.cancel()
     }
 
     fun addInput(input: E, flowInput: Flow<T>) {
--- src/jvmTest/kotlin/com/amuza/kotlin/flowvalve/FlowSwitchTest.kt
+++ src/jvmTest/kotlin/com/amuza/kotlin/flowvalve/FlowSwitchTest.kt
@@ -1,10 +1,12 @@
 package com.amuza.kotlin.flowvalve
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class FlowSwitchTest {
@@ -16,7 +18,7 @@ class FlowSwitchTest {
     fun testFlowSwitch_withoutInput_shouldGiveDefaultValue() = runBlockingTest {
         val flowSwitch = FlowSwitch(TestEnum::values, "value", this)
 
-        assertEquals("value", flowSwitch.outFlow.value)
+        assertEquals("value", flowSwitch.outFlow.first())
     }
 
     @Test
@@ -25,7 +27,7 @@ class FlowSwitchTest {
 
         flowSwitch.selectInput(TestEnum.A)
 
-        assertEquals("value", flowSwitch.outFlow.value)
+        assertEquals("value", flowSwitch.outFlow.first())
     }
 
     @Test
@@ -38,7 +40,7 @@ class FlowSwitchTest {
 
         flowSwitch.selectInput(TestEnum.A)
 
-        assertEquals("another value", flowSwitch.outFlow.value)
+        assertEquals("another value", flowSwitch.outFlow.first())
     }
 
     @Test
@@ -53,12 +55,23 @@ class FlowSwitchTest {
         })
 
         flowSwitch.selectInput(TestEnum.A)
-        assertEquals("value", flowSwitch.outFlow.value)
+        assertEquals("value", flowSwitch.outFlow.first())
 
         flowSwitch.selectInput(TestEnum.C)
-        assertEquals("Value from C", flowSwitch.outFlow.value)
+        assertEquals("Value from C", flowSwitch.outFlow.first())
 
         flowSwitch.selectInput(TestEnum.B)
-        assertEquals("Value from B", flowSwitch.outFlow.value)
+        assertEquals("Value from B", flowSwitch.outFlow.first())
+    }
+
+    @Test
+    fun testFlowSwitch_dontStartFlow_WhenNotSelectedInput() = runBlockingTest {
+        val flowSwitch = FlowSwitch(TestEnum::values, "value", this)
+
+        flowSwitch.addInput(TestEnum.B, flow<String> {
+            fail("Flow should not start.")
+        })
+
+        flowSwitch.selectInput(TestEnum.B)
     }
 }
