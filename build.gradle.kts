ad9bb3d23d55a7650c1cd98c49de858e82447654
Peter Fortuin <peter.fortuin@gmail.com>
2021-08-01 12:06:43 +0200

    Removed publishing tasks from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ object DependencyVersions {
 plugins {
     kotlin("multiplatform") version "1.4.31"
     `maven-publish`
-    id("com.jfrog.bintray") version "1.8.5"
 }
+
 group = "com.amuza.kotlin"
 version = "1.0.3-SNAPSHOT"
 
@@ -68,95 +68,4 @@ kotlin {
     }
 
     explicitApi()
-}
-
-val sourcesJar by tasks.creating(Jar::class) {
-    archiveClassifier.set("sources")
-    from(kotlin.sourceSets.getByName("commonMain").kotlin.sourceDirectories)
-}
-
-val artifactName = "flow-valve"
-val artifactGroup = project.group.toString()
-val artifactVersion = project.version.toString()
-
-val pomUrl = "https://github.com/AmuzaNL/FlowValve"
-val pomScmUrl = "https://github.com/AmuzaNL/FlowValve"
-val pomIssueUrl = "https://github.com/AmuzaNL/FlowValve/issues"
-val pomDesc = "https://github.com/AmuzaNL/FlowValve"
-
-val githubRepo = "AmuzaNL/FlowValve"
-val githubReadme = "README.md"
-
-val pomLicenseName = "MIT"
-val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
-val pomLicenseDist = "repo"
-
-val pomDeveloperId = "peter-fortuin"
-val pomDeveloperName = "Peter Fortuin"
-
-publishing {
-    publications {
-        create<MavenPublication>("flow-valve") {
-            groupId = artifactGroup
-            artifactId = artifactName
-            version = artifactVersion
-
-            artifact("$buildDir/libs/${project.name}-jvm-${project.version}.jar")
-            artifact(sourcesJar)
-
-            pom.withXml {
-                asNode().apply {
-                    appendNode("description", pomDesc)
-                    appendNode("name", rootProject.name)
-                    appendNode("url", pomUrl)
-                    appendNode("licenses").appendNode("license").apply {
-                        appendNode("name", pomLicenseName)
-                        appendNode("url", pomLicenseUrl)
-                        appendNode("distribution", pomLicenseDist)
-                    }
-                    appendNode("developers").appendNode("developer").apply {
-                        appendNode("id", pomDeveloperId)
-                        appendNode("name", pomDeveloperName)
-                    }
-                    appendNode("scm").apply {
-                        appendNode("url", pomScmUrl)
-                    }
-                }
-            }
-        }
-    }
-}
-
-bintray {
-    user = project.findProperty("bintrayUser").toString()
-    key = project.findProperty("bintrayKey").toString()
-    publish = true
-
-    setPublications("flow-valve")
-
-    pkg.apply {
-        repo = "kotlin"
-        name = artifactName
-        userOrg = "amuza"
-        vcsUrl = pomScmUrl
-        description = "Added functionality to Kotlin Flow"
-        setLabels("kotlin", "flow")
-        setLicenses("MIT")
-        desc = description
-        websiteUrl = pomUrl
-        issueTrackerUrl = pomIssueUrl
-        githubRepo = "AmuzaNL/FlowValve"
-        githubReleaseNotesFile = githubReadme
-
-        version.apply {
-            name = artifactVersion
-            desc = pomDesc
-            vcsTag = artifactVersion
-        }
-    }
-}
-
-tasks.named("bintrayUpload") {
-    dependsOn(sourcesJar)
-    dependsOn(tasks.named("jvmJar"))
 }
